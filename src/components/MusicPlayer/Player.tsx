71448c0d9042ac8c2b0c878e9fa4a8a93703118c
Jackson Merle <jackson.merle@gmail.com>
2024-10-30 14:52:55 +0900

    fix: music player defaults to dark theme

--- src/components/MusicPlayer/Player.tsx
+++ src/components/MusicPlayer/Player.tsx
@@ -35,6 +35,9 @@ export default function Player () {
             placement={{
                 player: "bottom",
                 interface: interfacePlacement,
+            }}
+            rootContainerProps={{
+                colorScheme: "dark",
             }}/>
         )
 }
